62d2ec03079aa79e5b739f5ef8b8afc65b931f17
abhinav3254 <abhinav3254@gmail.com>
2025-05-18 18:37:49 +0530

    intial commit

---
+++ src/test/java/com/abhinav3254/webflux/WebfluxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abhinav3254.webflux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebfluxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
