688f542abb14ead9f40a1cb348d4babc0d8ad870
abhinav3254 <abhinav3254@gmail.com>
2025-05-19 21:39:38 +0530

    enable package scan for bean creation

--- src/main/java/com/abhinav3254/webflux/WebfluxApplication.java
+++ src/main/java/com/abhinav3254/webflux/WebfluxApplication.java
@@ -2,8 +2,14 @@ package com.abhinav3254.webflux;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.r2dbc.repository.config.EnableR2dbcRepositories;
 
-@SpringBootApplication
+
+// this we are telling spring to scan which package and this ${sec} we will passing
+// using our application.properties
+@SpringBootApplication(scanBasePackages = "com.abhinav3254.webflux.${sec}")
+// r2dbc have their own they don't understand spring properties
+@EnableR2dbcRepositories(basePackages = "com.abhinav3254.webflux.${sec}")
 public class WebfluxApplication {
 
 	public static void main(String[] args) {
