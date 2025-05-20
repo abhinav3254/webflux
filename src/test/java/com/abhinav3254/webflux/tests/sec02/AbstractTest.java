f6166872779107478027368f03524473ec34ff1b
abhinav3254 <abhinav3254@gmail.com>
2025-05-20 06:22:30 +0530

    setting log level to debug

--- src/test/java/com/abhinav3254/webflux/tests/sec02/AbstractTest.java
+++ src/test/java/com/abhinav3254/webflux/tests/sec02/AbstractTest.java
@@ -2,6 +2,9 @@ package com.abhinav3254.webflux.tests.sec02;
 
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest(properties = "sec=sec02")
+@SpringBootTest(properties = {
+        "sec=sec02",
+        "logging.level.org.springframework.r2dbc=DEBUG"
+})
 public abstract class AbstractTest {
 }
