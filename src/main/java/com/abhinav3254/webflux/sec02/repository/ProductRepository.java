c6dffccfca455ccefec4fc7a18ddca6e66b60423
abhinav3254 <abhinav3254@gmail.com>
2025-05-20 06:47:49 +0530

    pageable

--- src/main/java/com/abhinav3254/webflux/sec02/repository/ProductRepository.java
+++ src/main/java/com/abhinav3254/webflux/sec02/repository/ProductRepository.java
@@ -1,6 +1,7 @@
 package com.abhinav3254.webflux.sec02.repository;
 
 import com.abhinav3254.webflux.sec02.entity.Product;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 import org.springframework.stereotype.Repository;
 import reactor.core.publisher.Flux;
@@ -10,5 +11,8 @@ import reactor.core.publisher.Flux;
 public interface ProductRepository extends ReactiveCrudRepository<Product,Integer> {
 
     Flux<Product> findByPriceBetween(int from,int to);
+
+//    findAllBy and findBy are same
+    Flux<Product> findBy(Pageable pageable);
 
 }
--- src/test/java/com/abhinav3254/webflux/tests/sec02/Lec02ProductRepositoryTest.java
+++ src/test/java/com/abhinav3254/webflux/tests/sec02/Lec02ProductRepositoryTest.java
@@ -1,10 +1,13 @@
 package com.abhinav3254.webflux.tests.sec02;
 
 import com.abhinav3254.webflux.sec02.repository.ProductRepository;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import reactor.test.StepVerifier;
 
 public class Lec02ProductRepositoryTest extends AbstractTest {
@@ -22,6 +25,18 @@ public class Lec02ProductRepositoryTest extends AbstractTest {
                     .expectNextCount(3)
                     .expectComplete()
                     .verify();
+    }
+
+    @Test
+    public void pageable() {
+        this.productRepository.findBy(PageRequest.of(0,3).withSort(Sort.by("price").ascending()))
+                .doOnNext(p->log.info("{}",p))
+                .as(StepVerifier::create)
+                .assertNext(p-> Assertions.assertEquals(200,p.getPrice()))
+                .assertNext(p-> Assertions.assertEquals(250,p.getPrice()))
+                .assertNext(p-> Assertions.assertEquals(300,p.getPrice()))
+                .expectComplete()
+                .verify();
     }
 
 }
