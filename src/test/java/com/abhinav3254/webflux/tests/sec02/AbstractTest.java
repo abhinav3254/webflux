7da18d285b61a9f2eedfedab86127752072d8e91
abhinav3254 <abhinav3254@gmail.com>
2025-05-20 06:22:58 +0530

    show sql commands

--- src/test/java/com/abhinav3254/webflux/tests/sec02/AbstractTest.java
+++ src/test/java/com/abhinav3254/webflux/tests/sec02/AbstractTest.java
@@ -2,6 +2,8 @@ package com.abhinav3254.webflux.tests.sec02;
 
 import org.springframework.boot.test.context.SpringBootTest;
 
+
+// logging.level.org.springframework.r2dbc=DEBUG this will sql commands
 @SpringBootTest(properties = {
         "sec=sec02",
         "logging.level.org.springframework.r2dbc=DEBUG"
